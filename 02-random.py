5c9acba609d737f9fc817e9711bfae1157b96fee
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-08 00:29:36 -0500

    04-randon

---
+++ 02-random.py
@@ -0,0 +1,14 @@
+
+# random 
+import random
+aleatorio =  random.randrange(1,10)
+print(f"aleatorio: {aleatorio}")
+
+aleatorio_02 =  random.randrange(1,11,2) # va de 2 en 2 
+print(f"aleatorio: {aleatorio_02}")
+
+aleatorio_entero = random.randint(1,12) #valor entero
+print(aleatorio_entero)
+aleatorio_entero = random.uniform(1.23,10.2) #valor decimal
+print(aleatorio_entero)
+
