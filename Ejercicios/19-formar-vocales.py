7e08a39f38ba48778cb96b2114dda06340a8bf9c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 13:15:08 -0500

    ejercicio: Imprimir un Patrón bucle for

---
+++ Ejercicios/19-formar-vocales.py
@@ -0,0 +1,15 @@
+"""
+Imprimir un Patrón
+Usa un bucle for para imprimir el siguiente patrón:
+*
+**
+***
+****
+*****
+ """
+ 
+caracter = "*"
+longitud= 5
+for x in range(longitud+1):
+    print(caracter*x)
+ 
