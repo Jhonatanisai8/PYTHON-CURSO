7c78c36cd6a510f340db43ae4cffd6a4d0bf16ce
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 12:39:38 -0500

    ejercicio: Suma de Números bucle for

---
+++ Ejercicios/12-suma-numeros-for.py
@@ -0,0 +1,11 @@
+"""
+Suma de Números
+Calcula la suma de los números del 1 
+al 100 utilizando un bucle for.
+"""
+
+suma  = 0
+
+for x in range(1,101):
+    suma += x
+print(f"Suma del 0 al 100: {suma}")
