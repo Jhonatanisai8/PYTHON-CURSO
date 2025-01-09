574a5c68930cc5aa2cfbbfced622b4dc6cfe7dc6
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 12:43:15 -0500

    ejercicio: Lista de Nombres bucle for

---
+++ Ejercicios/15-saludo-elementos.py
@@ -0,0 +1,12 @@
+"""
+Lista de Nombres
+Dada una lista de nombres como 
+["Ana", "Luis", "Pedro", "María"],
+utiliza un bucle for para imprimir 
+un saludo personalizado para cada 
+nombre (por ejemplo, "Hola, Ana").
+"""
+lista = ["Ana", "Luis", "Pedro", "María"]
+
+for nombre in lista:
+    print(f"Hola {nombre}")
