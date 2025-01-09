62d6237b6c7c1ee826d1524fe86d79f9c3296ddc
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 09:09:38 -0500

    Escribe un programa que pida al usuario su edad y determine si puede votar (mayor o igual a 18 años).

---
+++ Ejercicios/02-mayor-edad.py
@@ -0,0 +1,10 @@
+"""
+Escribe un programa que pida al usuario su edad y determine si puede votar (mayor o igual a 18 años).
+"""
+
+edad = 11
+if edad >= 18:
+    print("Puedes Votar")
+else:
+    print("No Puedes Votar")
+    
