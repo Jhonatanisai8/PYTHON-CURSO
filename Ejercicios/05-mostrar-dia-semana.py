a0cf5709867bc47becfcda0697c9c86e15f91f2c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 09:23:20 -0500

    mostrar el dia de la semana

---
+++ Ejercicios/05-mostrar-dia-semana.py
@@ -0,0 +1,25 @@
+"""
+Escribe un programa que solicite un número del 1 al 7 y muestre el día de la semana correspondiente (1 para lunes, 2 para martes, etc.). Si el número no está en el rango, muestra un mensaje de error.
+"""
+
+numero = 4
+
+if numero == 1:
+    print("Lunes")
+elif numero == 2:
+    print("Martes")
+elif numero == 3:
+    print("Miercoles")
+elif numero == 4:
+    print("Jueves")
+elif numero == 5:
+    print("Viernes")
+elif numero == 6:
+    print("Sabado")
+elif numero == 7:
+    print("Domingo")
+elif numero >= 7:
+    print("Numero no valido!")
+    
+    
+    
