555a76a7402952c850e72f1a950432219fbbfd8a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-10 09:42:58 -0500

    ejercicio: crear una lista

---
+++ Ejercicios/20-crear-lista.py
@@ -0,0 +1,10 @@
+"""
+1. Crear una lista
+Crea una lista con al menos 5 elementos 
+(pueden ser números, cadenas o ambos).
+Imprime la lista completa y su longitud.
+"""
+
+lista = [1,2,3,4,5]
+print(lista)
+print(len(lista))
