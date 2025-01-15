32ee8014f955417f58ae3e2de87a9e50818565ab
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-15 13:39:01 -0500

    creacion de tuplas

---
+++ 10-tuplas/01-creacion.py
@@ -0,0 +1,9 @@
+# creacion de tuplas
+my_tuple = ("apple", "banana", "cherry")
+print(my_tuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+tuple1 = ("abc", 34, True, 40, "male")
+print(tuple1)
