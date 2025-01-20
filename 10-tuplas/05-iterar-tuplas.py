c5f51b6689b30b99068085b7c93c708bf18be40f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-19 22:25:45 -0500

    iterar tuplas con while

--- 10-tuplas/05-iterar-tuplas.py
+++ 10-tuplas/05-iterar-tuplas.py
@@ -9,3 +9,10 @@ paises  = ("peru","colombia","paraguay")
 for i in range(len(paises)):
     print(paises[i])
     
+    
+print("")
+frutas = ("apple", "banana", "cherry")
+i = 0
+while i < len(frutas):
+    print(frutas[i])
+    i= i +1
