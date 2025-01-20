8ee42892e5b7cad194606aa9e8d1dcaa3948e78f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-19 22:18:56 -0500

    # iteracion con su determinado indice

--- 10-tuplas/05-iterar-tuplas.py
+++ 10-tuplas/05-iterar-tuplas.py
@@ -2,4 +2,10 @@
 thistuple = ("apple", "banana", "cherry")
 for fruta in thistuple:
     print(fruta)
+
+# iteracion con su determinado indice
+print("")
+paises  = ("peru","colombia","paraguay")
+for i in range(len(paises)):
+    print(paises[i])
     
